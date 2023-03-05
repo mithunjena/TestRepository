e040b62ece385eee0af2cb2afd6ab52786753c74
Mithun <Mithun@Mithun-PC>
2023-03-05 09:11:02 +0530

    code

--- TestProjectEclipse/src/javaPackage/TestClass.java
+++ TestProjectEclipse/src/javaPackage/TestClass.java
@@ -6,6 +6,7 @@ public class TestClass {
 		System.out.println("I love my family.");
 		System.out.println("Hello World");
 		System.out.println("Nuapalli");
+		System.out.println("java, selenium, python, TestNG");
 	}
 	
 
