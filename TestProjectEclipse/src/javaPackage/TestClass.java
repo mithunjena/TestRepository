262fe9f5d4ac09118d220dd9a64bda2eb2c34cb5
Mithun <Mithun@Mithun-PC>
2023-03-05 08:32:12 +0530

    Added print statement

--- TestProjectEclipse/src/javaPackage/TestClass.java
+++ TestProjectEclipse/src/javaPackage/TestClass.java
@@ -3,6 +3,7 @@ package javaPackage;
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Mithun my dear.");
+		System.out.println("I love my family.");
 	}
 	
 
